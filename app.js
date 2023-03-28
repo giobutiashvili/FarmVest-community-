e9b797970d2b85e13aeb1e0a77771f4e04bf3b47
giorgi.b <100768279+giobutiashvili@users.noreply.github.com>
2023-03-28 16:17:15 +0400

    add js functions

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+//   burger menu function
+function burgerMenu() {
+    document.querySelector('.nav').classList.toggle('active');
+    document.getElementById('menuIcon').style.transform =
+      document.querySelector('.nav').classList.contains('active') ? 'rotate(90deg)' : 'rotate(0deg)';
+    document.getElementById('menuIcon').style.transition = '1.3s';
+  }
+  
+
+//  shop list mouse hover function 
+
+document.querySelectorAll('.shop-list a').forEach(item => {
+    item.addEventListener('onmouseover', () => item.style.width = '300px');
+    item.addEventListener('onmouseout', () => item.style.width = 'auto');
+});
+ 
+
+// accordion menu function
+
 const acordion = document.querySelectorAll('.acardion .acardion-card');
 acordion.forEach(foo =>{
     foo.onclick = () => {
@@ -6,14 +25,23 @@ acordion.forEach(foo =>{
     }
 })
 
-function burgerMenu() {
-    let menu = document.querySelector(".nav")
-    menu.classList.toggle('active')
-    if( menu.classList.contains("active")){
-        document.getElementById('menuIcon').style.transform = 'rotate(90deg)';
-        document.getElementById('menuIcon').style.transition = '1.3s';
-    }else{
-        document.getElementById('menuIcon').style.transform = 'rotate(0deg)'
-        document.getElementById('menuIcon').style.transition = '1.3s';
-    }
-}
+// accordion menu box-shadow function
+
+document.querySelectorAll('.acardion-card').forEach(card => {
+    card.addEventListener('mouseover', () => card.style.boxShadow = 'inset 0 0 10px #388E3C');
+    card.addEventListener('mouseout', () => card.style.boxShadow = 'none');
+  });
+  
+
+
+
+const svgElement = document.querySelector('#my-svg');
+
+svgElement.addEventListener('mouseenter', () => {
+svgElement.setAttribute('fill', 'red');
+});
+
+svgElement.addEventListener('mouseleave', () => {
+svgElement.removeAttribute('fill');
+});
+
